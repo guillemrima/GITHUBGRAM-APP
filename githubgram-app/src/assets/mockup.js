fc002dddeba4ab4ed376dd1291403a5522183a9d
guillemrima <117720260+guillemrima@users.noreply.github.com>
2023-02-20 16:58:13 +0100

    Feat: Complete Profile Component Mockup

--- githubgram-app/src/assets/mockup.js
+++ githubgram-app/src/assets/mockup.js
@@ -1,5 +1,8 @@
 import avatar from "./img/avatar.jpg"
 
 export default {
-    "avatar": avatar
+    "avatar": avatar,
+    "nickname": "guillemrima",
+    "name": "Guillem Rivas Martorell",
+    "bio": "🏆 Junior front-end developer. 📖Currently learnig Javascript. 🌴Mallorca, Spain."
 }
--- githubgram-app/src/components/Profile.jsx
+++ githubgram-app/src/components/Profile.jsx
@@ -4,35 +4,42 @@ import mockup from "./../assets/mockup";
 
 export default function Profile() {
     return (
-        <section className={style.profileContainer}>
-            <div className={style.avatar}>
-                <img src={mockup.avatar} />
+        <section className={style.profileSection}>
+            <div className={style.profileContainer}>
+                <div className={style.avatar}>
+                    <img src={mockup.avatar} />
+                </div>
+                <div className={style.stats}>
+                    <div className={style.info}>
+                        <div className={style.number}>
+                            <p>35</p>
+                        </div>
+                        <div className={style.text}>
+                            <p>Repositories</p>
+                        </div>
+                    </div>
+                    <div className={style.info}>
+                        <div className={style.number}>
+                            <p>761</p>
+                        </div>
+                        <div className={style.text}>
+                            <p>Followers</p>
+                        </div>
+                    </div>
+                    <div className={style.info}>
+                        <div className={style.number}>
+                            <p>1.118</p>
+                        </div>
+                        <div className={style.text}>
+                            <p>Following</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={style.stats}>
-                <div className={style.info}>
-                    <div className={style.number}>
-                        <p>35</p>
-                    </div>
-                    <div className={style.text}>
-                        <p>Repositories</p>
-                    </div>
-                </div>
-                <div className={style.info}>
-                    <div className={style.number}>
-                        <p>761</p>
-                    </div>
-                    <div className={style.text}>
-                        <p>Followers</p>
-                    </div>
-                </div>
-                <div className={style.info}>
-                    <div className={style.number}>
-                        <p>1.118</p>
-                    </div>
-                    <div className={style.text}>
-                        <p>Following</p>
-                    </div>
-                </div>
+            <div className={style.descriptionContainer}>
+                <h1 className={style.name}>{mockup.name}</h1>
+                <p className={style.nickname}>{mockup.nickname}</p>
+                <p className={style.bio}>{mockup.bio}</p>
             </div>
         </section>
     )
